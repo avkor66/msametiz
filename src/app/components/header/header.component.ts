c3f9dc9172d1871bbaa7d16783ee1310d6826798
Artem Korotkov <avkor66@gmail.com>
2024-02-19 14:08:41 +0500

    fix: flexible header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 const logo = '../../../assets/data/images/logo.svg'
+const tgl = '../../../assets/data/images/icons/btn-toggle.svg'
 
 @Component({
   selector: 'app-header',
@@ -9,4 +10,50 @@ const logo = '../../../assets/data/images/logo.svg'
 
 export class HeaderComponent {
   logo = logo
+  tgl = tgl
+  
+  // const btnToggle = document.getElementById('btn-toggle')
+  // const menuToggle = document.getElementById('toggle-menu')
+  // const navMenuItem = document.querySelectorAll('.nav-menu-item')
+  // let flag = false
+  // let rand = Math.floor(Math.random() * 8)
+  
+  // window.onscroll = function(){
+  //     return false;
+  //   }
+  // btnToggle.addEventListener('click', () => {
+  //     if (flag) {
+  //         menuToggle.classList.add('transition-toggle-menu-leave')
+  //         menuToggle.classList.remove('transition-toggle-menu')
+  //         btnToggle.classList.remove('rotate')
+  //         btnToggle.classList.add('rotate-leave')
+  //         flag = false
+  //         document.querySelector('.modal__menu').remove()
+  //     } else {
+  //         menuToggle.classList.add('transition-toggle-menu')
+  //         btnToggle.classList.add('rotate')
+  //         btnToggle.classList.remove('rotate-leave')
+  //         flag = true
+  //         const cardPets_ = document.createElement('div')
+  //         cardPets_.classList.add('modal__menu')
+  //         document.body.insertAdjacentElement('afterbegin', cardPets_)
+     
+  //         document.querySelector('.modal__menu').addEventListener('click', () => {
+  //             menuToggle.classList.add('transition-toggle-menu-leave')
+  
+  //             menuToggle.classList.remove('transition-toggle-menu')
+  //             btnToggle.classList.remove('rotate')
+  //             btnToggle.classList.add('rotate-leave')
+  //             flag = false
+  //             document.querySelector('.modal__menu').remove()    
+  //         })
+  
+  //     }
+  // })
+  // menuToggle.addEventListener('animationend', () => {
+  //     menuToggle.classList.remove('transition-toggle-menu-leave')
+  
+  // })
+
+
 }
